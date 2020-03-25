e98924446de43b98a69d784bdab351f99869e7fe
Martin Budinský <martinbudinsky3@github.com>
2020-03-25 11:49:08 +0100

    final verzia

--- 8_puzzle_greedy_algo.py
+++ 8_puzzle_greedy_algo.py
@@ -1,12 +1,19 @@
 import math
 import heapq
 import copy
+import time
 
+PRINT = 0
+
+# min heap to store generated nodes, key is heuristic distance estimation
 generated_nodes = []
+# list to store all processed states to avoid duplicates while generating new states
 processed_states = []
+# dictionary to store values:coordinates pairs of goal state
 goal_dict = {}
 
 
+# class representing node
 class Node:
     def __init__(self, state, parent, last_move, dist):
         self.state = state
@@ -18,6 +25,7 @@ class Node:
         return self.dist < other.dist
 
 
+# heuristic that returns number of wrong placed tiles
 def heuristic1(actual_state, goal_state, m, n):
     wrong_tiles = 0
 
@@ -29,6 +37,7 @@ def heuristic1(actual_state, goal_state, m, n):
     return wrong_tiles
 
 
+# heuristic that returns sum of manhattan distances
 def heuristic2(actual_state, m, n):
     manhattan_distance = 0
 
@@ -45,6 +54,7 @@ def heuristic2(actual_state, m, n):
     return manhattan_distance
 
 
+# function that returns x, y coordinates of blank tile
 def find_blank(state, m, n):
     for i in range(n):
         for j in range(m):
@@ -54,6 +64,7 @@ def find_blank(state, m, n):
     return None
 
 
+# function that swap tiles
 def swap(state, x_blank, y_blank, x_neigh, y_neigh):
     temp = state[y_blank][x_blank]
     state[y_blank][x_blank] = state[y_neigh][x_neigh]
@@ -62,6 +73,7 @@ def swap(state, x_blank, y_blank, x_neigh, y_neigh):
     return state
 
 
+# functions that make moves
 def move_up(state, m, n):
     x_blank, y_blank = find_blank(state, m, n)
     if y_blank != n - 1:
@@ -102,6 +114,7 @@ def move_left(state, m, n):
     return None
 
 
+# function that return estimated distance based on input heuristic
 def choose_heuristic(state, goal_state, m, n, heur):
     dist = 0
     if heur == 1:
@@ -112,6 +125,7 @@ def choose_heuristic(state, goal_state, m, n, heur):
     return dist
 
 
+# function that create node and push it to heap of generated nodes
 def create_node(state, goal_state, m, n, parent, last_move, heur):
     dist = choose_heuristic(state, goal_state, m, n, heur)
     node = Node(state, parent, last_move, dist)
@@ -119,15 +133,18 @@ def create_node(state, goal_state, m, n, parent, last_move, heur):
     heapq.heappush(generated_nodes, node)
 
 
+# function that generates all possible new nodes
 def generate_nodes(node, goal_state, m, n, heur):
     state = node.state
     last_move = node.last_move
 
+    # generating new states
     up_state = move_up(state, m, n)
     down_state = move_down(state, m, n)
     right_state = move_right(state, m, n)
     left_state = move_left(state, m, n)
 
+    # checking if states were not processed and if used operator was valid
     if up_state not in processed_states and up_state is not None and last_move != "D":
         create_node(up_state, goal_state, m, n, node, "U", heur)
 
@@ -141,26 +158,27 @@ def generate_nodes(node, goal_state, m, n, heur):
         create_node(left_state, goal_state, m, n, node, "L", heur)
 
 
+# implementation of greedy algorithm
 def greedy(init_state, goal_state, m, n, heur):
-    number_of_steps = 0
-
-    create_node(init_state, goal_state, m, n, None, None, heur)
+    create_node(init_state, goal_state, m, n, None, None, heur)  # creating first init node, creating a min heap
     while generated_nodes:
-        print("Generated nodes:", len(generated_nodes))
-        print("Processed nodes:", len(processed_states))
-        print()
+        if PRINT:
+            print("Generated nodes:", len(generated_nodes))
+            print("Processed nodes:", len(processed_states))
+            print()
 
-        actual_node = heapq.heappop(generated_nodes)
+        actual_node = heapq.heappop(generated_nodes)  # extracting min node from heap
         actual_state = actual_node.state
-        processed_states.append(actual_state)
-        if actual_state == goal_state:
+        processed_states.append(actual_state)  # storing state as processed
+        if actual_state == goal_state:  # if goal_state was found function returns node with goal state
             return actual_node;
 
-        generate_nodes(actual_node, goal_state, m, n, heur)
+        generate_nodes(actual_node, goal_state, m, n, heur)  # generating all possible succesors
 
-    return None
+    return None  # if algorithm didn't find goal solution function returns None
 
 
+# functions to read and process input
 def create_goal_state_dict(goal_state, m, n):
     for i in range(n):
         for j in range(m):
@@ -168,14 +186,12 @@ def create_goal_state_dict(goal_state, m, n):
             goal_dict[tile] = (j, i)
 
 
-def read_state(f, m, n):
+def read_state(f, n):
     state = []
-    row = []
 
     for i in range(n):
         line = f.readline()
-        for j in range(m):
-            row.append(line[j * 2])
+        row = line.split()
         state.append(row)
         row = []
 
@@ -190,34 +206,49 @@ def read_input(file):
     m = int(first_line[2])
     n = int(first_line[4])
 
-    init_state = read_state(f, m, n)
+    init_state = read_state(f, n)
     f.readline()
-    goal_state = read_state(f, m, n)
+    goal_state = read_state(f, n)
 
     return heur, m, n, init_state, goal_state
 
 
-def print_output(result_node):
+# function to format and print output
+def print_output(result_node, start, end):
+    if result_node is None:
+        print("Solution does not exist")
+        return
+
     op_dict = {"U": "UP", "D": "DOWN", "R": "RIGHT", "L": "LEFT"}
+
+    number_of_ops = 0
     operators = []
     actual_node = result_node
     while actual_node.parent:
         operators.append(actual_node.last_move)
         actual_node = actual_node.parent
+        number_of_ops += 1
 
     for i in range(len(operators) - 1, -1, -1):
         print(op_dict[operators[i]])
 
     print()
+    print("Number of moves:", number_of_ops)
     print("Total number of nodes:", len(generated_nodes) + len(processed_states))
+    print("Time: {} s".format(end - start))
 
 
-heur, m, n , init_state, goal_state = read_input("input.txt")
+start = time.perf_counter()
+# heur, m, n , init_state, goal_state = read_input("basic.txt")
+# heur, m, n , init_state, goal_state = read_input("basic_1.txt")
+# heur, m, n , init_state, goal_state = read_input("input_3x2.txt")
+# heur, m, n , init_state, goal_state = read_input("input_4x2.txt")
+heur, m, n , init_state, goal_state = read_input("input_4x3.txt")
+# heur, m, n , init_state, goal_state = read_input("input_5x2.txt")
 create_goal_state_dict(goal_state, m, n)
 result_node = greedy(init_state, goal_state, m, n, heur)
-print_output(result_node)
-
-# print(result_node.state)
+end = time.perf_counter()
+print_output(result_node, start, end)
 
 
 
